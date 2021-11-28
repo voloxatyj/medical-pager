a3ac4fa8f8f1e9e9fbfdf54561390122b3f49c69
voloxatyj <ostapmedunskij@gmail.com>
2021-11-28 22:44:15 +0200

    Add authentification for server side

---
+++ server/controllers/auth.js
@@ -0,0 +1,54 @@
+const { connect } = require('getstream');
+const bcrypt = require('bcrypt');
+const stream_chat = require('stream-chat');
+const crypto = require('crypto');
+
+const { api_key, api_secret, app_id } = require('../config');
+
+const signup = (req, res) => {
+	try {
+		const { fullName, userName, password, phoneNumber } = req.body;
+
+		const userId = crypto.randomBytes(16).toString('hex');
+
+		const serverClient = connect(api_key, api_secret, app_id);
+
+		const hashedPassword = await bcrypt.hash(password, 10);
+
+		const token = serverClient.createUserToken(userId);
+
+		res.status(200).json({ token, fullName, userName, userId, hashedPassword, phoneNumber });
+	} catch (error) {
+		console.log(error);
+		res.status(500).json({ message: error });
+	}
+};
+
+const login = () => {
+	try {
+		const { userName, password } = req.body;
+
+		const serverClient = connect(api_key, api_secret, app_id);
+
+		const client = stream_chat.getInstance(api_key, api_secret);
+
+		const { users } = await client.queryUsers({ name: userName });
+
+		if (!users.length) return res.status(400).json({ message: 'User not found' });
+
+		const success = await bcrypt.compare(password, users[0].hashedPassword);
+
+		const token = serverClient.createUserToken(users[0].id);
+
+		if (success) {
+			return res.status(200).json({ token, fullName: users[0].fullName, userName, userId: users[0].id });
+		} else {
+			return res.status(500).json({ message: 'Incorrect password' });
+		}
+	} catch (error) {
+		console.log(error);
+		res.status(500).json({ message: error });
+	}
+};
+
+module.exports = { signup, login };
