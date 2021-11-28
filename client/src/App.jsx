6078d6f6e48a2ca0a3a7b3bfddd415110b7777c9
voloxatyj <ostapmedunskij@gmail.com>
2021-11-28 16:42:27 +0200

    Add Sidebar, styles and assets folder with images

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import { StreamChat } from 'stream-chat';
+import { Chat } from 'stream-chat-react';
+import Cookies from 'universal-cookie';
+import { ChannelContainer, ChannelListContainer } from './components';
+import './App.css';
+
+const apiKey = 'rkamxg3b4dvq';
+
+const client = StreamChat.getInstance(apiKey);
 
 const App = () => {
 	return (
-		<div>
-			<h1>Medical Pager Chat App</h1>
+		<div className="app__wrapper">
+			<Chat client={client} theme="team light">
+				<ChannelListContainer />
+				<ChannelContainer />
+			</Chat>
 		</div>
 	)
 }
