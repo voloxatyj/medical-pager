75a4718bdd9fb86244e5991914f03ad051b8cb93
voloxatyj <ostapmedunskij@gmail.com>
2021-12-27 13:50:38 +0200

    Change username as in front

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -7,7 +7,7 @@ const { api_key, api_secret, app_id } = require('../config/config');
 
 const signup = async (req, res) => {
 	try {
-		const { fullName, userName, password, phoneNumber } = req.body;
+		const { fullName, username, password, phoneNumber } = req.body;
 
 		const userId = crypto.randomBytes(16).toString('hex');
 
@@ -17,22 +17,22 @@ const signup = async (req, res) => {
 
 		const token = serverClient.createUserToken(userId);
 
-		res.status(200).json({ token, fullName, userName, userId, hashedPassword, phoneNumber });
+		res.status(200).json({ token, fullName, username, userId, hashedPassword, phoneNumber });
 	} catch (error) {
 		console.log(error);
 		res.status(500).json({ message: error });
 	}
 };
 
-const login = async () => {
+const login = async (req, res) => {
 	try {
-		const { userName, password } = req.body;
+		const { username, password } = req.body;
 
 		const serverClient = connect(api_key, api_secret, app_id);
 
 		const client = stream_chat.getInstance(api_key, api_secret);
 
-		const { users } = await client.queryUsers({ name: userName });
+		const { users } = await client.queryUsers({ name: username });
 
 		if (!users.length) return res.status(400).json({ message: 'User not found' });
 
@@ -41,7 +41,7 @@ const login = async () => {
 		const token = serverClient.createUserToken(users[0].id);
 
 		if (success) {
-			return res.status(200).json({ token, fullName: users[0].fullName, userName, userId: users[0].id });
+			return res.status(200).json({ token, fullName: users[0].fullName, username, userId: users[0].id });
 		} else {
 			return res.status(500).json({ message: 'Incorrect password' });
 		}
