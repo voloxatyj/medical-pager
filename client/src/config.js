443fbe44120311076ce2586d86f7929a69eef188
voloxatyj <ostapmedunskij@gmail.com>
2022-01-02 13:57:32 +0200

    Add production  adress backend server

--- client/src/config.js
+++ client/src/config.js
@@ -2,7 +2,9 @@ const config = {};
 
 config.api_key_stream_chat = process.env.REACT_APP_API_KEY_STREAM_CHAT;
 
-config.server_host = process.env.REACT_APP_SERVER_HOST || 'http://localhost';
-config.server_port = process.env.REACT_APP_SERVER_PORT || 5000;
+config.app = process.env.REACT_APP_MODE || 'dev';
+
+config.server_host = process.env.REACT_APP_SERVER_HOST || config.app === 'dev' ? 'http://localhost' : 'https://morning-mountain-13643.herokuapp.com';
+config.server_port = process.env.REACT_APP_SERVER_PORT || config.app === 'dev' ? 5000 : '';
 
 module.exports = config;
