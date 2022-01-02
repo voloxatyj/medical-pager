d0764bab9c8ddbada1da9f3ac9cd71b4da22b617
voloxatyj <ostapmedunskij@gmail.com>
2022-01-02 13:13:05 +0200

    add Twilio

--- server/config/config.js
+++ server/config/config.js
@@ -7,5 +7,8 @@ config.api_secret = process.env.STREAM_API_SECRET;
 config.api_key = process.env.STREAM_API_KEY;
 config.app_id = process.env.STREAM_APP_ID;
 config.app_dir = process.cwd();
+config.twilio_account_sid = process.env.TWILIO_ACCOUNT_SID;
+config.twilio_auth_token = process.env.TWILIO_AUTH_TOKEN;
+config.messaging_service_sid = process.env.MESSAGING_SERVICE_SID;
 
 module.exports = config;
--- server/index.js
+++ server/index.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const cors = require('cors');
+const { twilio_account_sid, twilio_auth_token, messaging_service_sid } = require('./config/config');
 
 const authRoutes = require('./routes/auth.js');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+const twilioClient = require('twilio')(twilio_account_sid, twilio_auth_token);
 
 app.use(cors());
 app.use(express.json());
@@ -12,6 +15,29 @@ app.use(express.urlencoded());
 
 app.get('/', (req, res) => {
 	res.send('Hello World!');
+});
+
+app.post('/', (req, res) => {
+	const { message, user: sender, type, members } = req.body;
+
+	if (type === 'message.new') {
+		members
+			.filter((member) => member.user_id !== sender.id) 
+			.forEach(({ user }) => {
+			if (!user.online) {
+				twilioClient.messages.create({
+					body: `You have a new message from ${message.user.fullName} - ${message.text}`,
+					messagingServiceSid: messaging_service_sid,
+					to: user.phoneNumber
+				})
+				.then(() => console.log('Message sent!'))
+				.catch((err) => console.log(err));
+			}
+		});
+		return res.status(200).send('Message sent!');
+	}
+
+	return res.status(200).send('Not a new message request!');
 });
 
 app.use('/auth', authRoutes);
